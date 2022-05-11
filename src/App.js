e83758e85d600334e82230c8a53298cdb24396b5
choiseungchul <tmdcjf152@naver.com>
2022-05-11 09:51:11 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 //common
 import Header from './components/common/Header';
@@ -22,7 +22,7 @@ function App() {
 	return (
 		<>
 
-<Switch>
+<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Route exact path='/'>
 				<Header type={'main'} />
 				<Visual />
@@ -33,7 +33,7 @@ function App() {
 				<Header type={'sub'} />
 
 			</Route>
-			</Switch>
+			</BrowserRouter>
 
 			<Route path='/department' component={Department} />
 			<Route path='/community' component={Community} />
