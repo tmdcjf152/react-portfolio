49b40cf1899c28f2688a2ce1204c57ca50d65a66
choiseungchul <tmdcjf152@naver.com>
2022-05-19 15:23:44 +0900

    메인 전면수정(완료)

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 function Footer() {
   return (
-<footer>
-  <div className="inner">
-    <p>2022 MINI &copy; ALL RIGHTS RESERVED</p>
-  </div>
-</footer>
+    <footer>
+      <div className="inner">
+        <div>
+          <h1>MINI</h1>
+          <p>MINIOFFICER-KR@NAVER.COM</p>
+          <p>+82(0) 02-8350-3384</p>
+          <address>100 Toegye-ro, Jung-gu, Seoul (Hoehyeon-dong)</address>
+        </div>
+        <p>2022 MINI &copy; ALL RIGHTS RESERVED</p>
+        <a href="#">TWITTER</a> <a href="#">FACEBOOK</a>       <a href="#">INSTAGRAM</a>
+      </div>
+    </footer>
   )
 }
 
